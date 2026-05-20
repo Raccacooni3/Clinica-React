7e3cceb38a0f6f5825b086f6a4c13ca846ca23d6
Raccacooni3 <lquinterofelipe@gmail.com>
2026-05-21 09:46:57 +1200

    Adding new active services

--- src/servicios.jsx
+++ src/servicios.jsx
@@ -2,9 +2,13 @@ function Servicios(props) {
   return (
     <div>
       <h2>Servicios</h2>
-      <ul>
-        {props.servicios.map((servicios) => (
-          <li key={servicios.id}>{servicios.title}</li>
+      <ul className="list-none p-0">
+        {props.servicios.map((servicio) => (
+          <li key={servicio.id}>
+            <button onClick={() => props.onSelect(servicio.title)}>
+              {servicio.title}
+            </button>
+          </li>
         ))}
       </ul>
     </div>
